bd9d5d849edae1397d3513941fd441ee47c8f7e9
Areeb-404 <mohdareebabdullah@gmail.com>
2026-08-22 18:29:48 +0000

    modularized and made a get_tasks() function
    made a function to print the tasks present in the dictionary to the
    output terminal

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 import json
 
+def get_tasks():
+    if not tasks:
+        print("\nYour task list is empty!")
+    for task in tasks:
+        status = "[X] " if task["completed"] else "[ ] "
+        print(f"{status}{task['id']}: {task['Title']}")
+
 tasks = [
     {"id" : 1, "Title" : "Buy Groceries", "completed" : True},
     {"id" : 2, "Title" : "Study Python Basics", "completed" : False}
@@ -8,10 +15,7 @@ tasks = [
 def main():
     print("------Task Manager------")
     print(f"Loaded {len(tasks)} tasks into memory.\n")
-
-    for task in tasks:
-        status = "[X] " if task["completed"] else "[ ] "
-        print(f"{status}{task['id']}: {task['Title']}")
+    get_tasks()
 
 if __name__ == "__main__":
     main()
