cd262e7103d75a42b134dcdb13fb02e584ecd6de
Areeb-404 <mohdareebabdullah@gmail.com>
2026-09-06 14:20:45 +0000

    feat(auth): expand database schema to include users table and task foreign key for authentication and jwt integration

--- database.py
+++ database.py
@@ -29,13 +29,28 @@ def get_db():
         connection.close()
 
 def create_tables():
-    query = """
+    # FOR THE USERS TABLE
+    create_users_table = """
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(100) UNIQUE NOT NULL,
+        hashed_password VARCHAR(255) NOT NULL);
+    """
+
+    # FOR THE TASKS TABLE
+    create_tasks_table = """
     CREATE TABLE IF NOT EXISTS tasks(
         id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         completed BOOLEAN DEFAULT FALSE
         );
     """
+
+    # Adding the user_id as a foreign key to tasks table to connect the two
+    migrate_tasks_table = """
+    ALTER TABLE tasks ADD COLUMN IF NOT EXISTS user_id INT REFERENCES users(id) ON DELETE CASCADE;
+    """
+
     connection = psycopg2.connect(
         host = DB_HOST,
         database = DB_NAME,
@@ -46,7 +61,10 @@ def create_tables():
     try:
         with connection:
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(create_users_table)
+                cursor.execute(create_tasks_table)
+                cursor.execute(migrate_tasks_table)
+
         print("Database Tables verified/created successfully!")
     finally:
         connection.close()
